07f0c5c7b4dc15491c4acd66ad9dfc887db6447d
Grisha Amiryan <grishaamiryan555@gmail.com>
2026-06-08 14:48:55 +0400

    Add HttpOnly Cookie auth middleware and update routes

---
+++ app/Http/Middleware/AddTokenFromCookieToHeader.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddTokenFromCookieToHeader
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!$request->hasHeader('Authorization') && $request->hasCookie('auth_token')){
+            $token = $request->cookie('auth_token');
+            $request->headers->set('Authorization', 'Bearer' . $token);
+        }
+        return $next($request);
+    }
+}
